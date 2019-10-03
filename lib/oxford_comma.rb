ef81f7361c1a6d6c0ec0ef5b5d85f67db1f04310
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-03 02:55:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  n = array.length
+  case n
+  when 1
+    array.join
+  when 2
+    array.join(" and ")
+  else
+    last_item = array.pop()
+    sentence = array.join(", ") + ", and #{last_item}"
+  end
 end
